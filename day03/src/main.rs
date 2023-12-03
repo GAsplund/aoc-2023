391c135c46588864a77a0f8d961966bb5f3cc7bb
Goostaf <gasplund2@gmail.com>
2023-12-03 16:59:41 +0100

    Day 3 optimizations

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -13,15 +13,15 @@ fn parse_map(map: Vec<String>) -> Vec<Vec<char>> {
         .collect()
 }
 
-fn get_numbers(map: Vec<Vec<char>>) -> (Vec<(u32, bool)>, Vec<Vec<u32>>) {
+fn get_numbers(map: Vec<Vec<char>>) -> (Vec<(u32, bool)>, Vec<(u32, u32)>) {
     let mut valid_num = false;
     let mut valid_gears: Vec<(usize, usize)> = Vec::new();
     let mut buf: String = String::from("");
-    let mut nums: Vec<(String, bool)> = Vec::new();
-    let mut gears: BTreeMap<(usize, usize), BTreeSet<String>> = BTreeMap::new();
+    let mut nums: Vec<(u32, bool)> = Vec::new();
+    let mut gears: BTreeMap<(usize, usize), BTreeSet<u32>> = BTreeMap::new();
 
     for (i, l) in map.iter().enumerate() {
-        for (j, c) in l.iter().enumerate() {
+        for (j, c) in l.iter().chain(&['.']).enumerate() {
             if c.is_ascii_alphanumeric() {
                 buf.push(*c);
                 let (is_part, mut gears_pos) = check_around(&map, j, i);
@@ -29,38 +29,26 @@ fn get_numbers(map: Vec<Vec<char>>) -> (Vec<(u32, bool)>, Vec<Vec<u32>>) {
 
                 valid_gears.append(&mut gears_pos);
             } else if !buf.is_empty() {
-                nums.push((buf.clone(), valid_num));
+                let num = buf.parse().unwrap();
+                nums.push((num, valid_num));
                 for g in &valid_gears {
                     let new = BTreeSet::new();
                     let pos = gears.entry(*g).or_insert_with(|| new);
-                    pos.insert(buf.clone());
+                    pos.insert(num);
                 }
                 buf.clear();
                 valid_gears.clear();
                 valid_num = false;
             }
         }
-
-        if !buf.is_empty() {
-            nums.push((buf.clone(), valid_num));
-        }
-        for g in &valid_gears {
-            let new = BTreeSet::new();
-            let pos = gears.entry(*g).or_insert_with(|| new);
-            pos.insert(buf.clone());
-        }
-        buf.clear();
-        valid_gears.clear();
-        valid_num = false;
     }
 
-    let number_parts = nums.iter().map(|n| (n.0.parse().unwrap(), n.1)).collect();
-    let gear_ratios = gears.values();
-    let gear_ratios = gear_ratios
+    let gear_ratios = gears
+        .values()
         .filter(|r| r.len() == 2)
-        .map(|g| g.iter().map(|p| p.parse::<u32>().unwrap()).collect())
+        .map(|g| (*g.first().unwrap(), *g.last().unwrap()))
         .collect();
-    (number_parts, gear_ratios)
+    (nums, gear_ratios)
 }
 
 fn is_symbol(c: char) -> bool {
@@ -68,29 +56,27 @@ fn is_symbol(c: char) -> bool {
 }
 
 fn get_at_offset(map: &Vec<Vec<char>>, x: i32, y: i32) -> char {
-    let y_l = map.get(y as usize);
-    if let Some(l) = y_l {
-        let x_c = l.get(x as usize);
-        return *x_c.unwrap_or(&'.');
-    }
-    '.'
+    let empty = Vec::new();
+    let l = map.get(y as usize).unwrap_or(&empty);
+    *l.get(x as usize).unwrap_or(&'.')
 }
 
+const OFFSETS: [(i32, i32); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
 fn check_around(map: &Vec<Vec<char>>, x: usize, y: usize) -> (bool, Vec<(usize, usize)>) {
-    let idx_offset: [(i32, i32); 8] = [
-        (-1, -1),
-        (-1, 0),
-        (-1, 1),
-        (0, -1),
-        (0, 1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-    ];
-    let symbol = idx_offset
+    let symbol = OFFSETS
         .iter()
         .any(|(x_o, y_o)| is_symbol(get_at_offset(map, x as i32 + x_o, y as i32 + y_o)));
-    let gears = idx_offset
+    let gears = OFFSETS
         .iter()
         .filter(|(x_o, y_o)| get_at_offset(map, x as i32 + x_o, y as i32 + y_o) == '*')
         .map(|(x_o, y_o)| ((x as i32 + x_o) as usize, (y as i32 + y_o) as usize))
@@ -111,6 +97,6 @@ fn main() {
     let sum: u32 = numbers.iter().sum();
     println!("Solution 1: {}", sum);
 
-    let ratios: u32 = gears.iter().map(|g| g[0] * g[1]).sum();
+    let ratios: u32 = gears.iter().map(|g| g.0 * g.1).sum();
     println!("Solution 2: {}", ratios);
 }
