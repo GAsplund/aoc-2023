c8ba9e90975007b732b6b3d4babe23ca5f5f952b
Goostaf <gasplund2@gmail.com>
2023-12-06 23:59:05 +0100

    Day 5 part 2
    Contains potential off-by-one error somehow

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,5 +1,5 @@
 fn read_file() -> Vec<String> {
-    include_str!("../test.txt")
+    include_str!("../input.txt")
         .split("\r\n\r\n")
         .map(|l| l.to_string())
         .collect()
@@ -22,30 +22,13 @@ impl MapObject {
         }
     }
 
-    pub fn map_ranged(&self, source_start: usize, len: usize) -> Vec<usize> {
-        let map_end = self.map_from + self.length;
-        let source_end = source_start + len;
-        if map_end > source_start && map_end < source_end {
-            // Intersection, left
-            let inside = self.map(source_start);
-            let outside = self.map(self.map_from);
-            vec![inside, outside]
-        } else if source_end > self.map_from && source_start < map_end {
-            // Intersection, right
-            let inside = self.map(source_start);
-            let outside = self.map(self.map_from);
-            vec![inside, outside]
-        } else if map_end > source_end && self.map_from < source_start {
-            // Contains
-            vec![self.map(source_start)]
-        } else if self.map_from > source_start && source_end > map_end {
-            // Contained
-            let outside = self.map(source_start);
-            let inside = self.map(self.map_from);
-            vec![inside, outside]
+    pub fn map_rev(&self, n: usize) -> usize {
+        let source_max = self.map_to + self.length;
+        if self.map_to <= n && n <= source_max {
+            let offset = n - self.map_to;
+            self.map_from + offset
         } else {
-            // Does not contain
-            vec![source_start]
+            n
         }
     }
 
@@ -55,6 +38,26 @@ impl MapObject {
     }
 }
 
+fn apply_map(n: usize, m: &Vec<MapObject>) -> usize {
+    let o = m.iter().find(|o| o.map(n) != n);
+    match o {
+        Some(obj) => obj.map(n),
+        None => n
+    }
+}
+
+fn apply_map_rev(n: usize, m: &Vec<MapObject>) -> usize {
+    let o = m.iter().find(|o| o.map_rev(n) != n);
+    match o {
+        Some(obj) => obj.map_rev(n),
+        None => n
+    }
+}
+
+fn within_range(n: usize, ranges: &Vec<&[usize]>) -> bool {
+    ranges.iter().any(|s| s[0] <= n && n <= (s[0] + s[1]))
+}
+
 
 fn main() {
     let map = read_file();
@@ -62,34 +65,28 @@ fn main() {
     let seeds: Vec<usize> = map.iter().next().unwrap().split_at(6).1.split_ascii_whitespace().map(|s| s.parse().unwrap()).collect();
     let maps: Vec<Vec<MapObject>> = map.iter().skip(1).map(|m| m.lines().into_iter().skip(1).map(|l| MapObject::from_str(l)).collect()).collect();
     let smallest_seed = seeds.iter().map(|s| {
-        let mut s_orig = *s;
         let mut s_new = *s;
         for m in maps.iter() {
-            let o = m.iter().find(|o| o.map(s_new) != s_orig);
-            s_new = match o {
-                Some(obj) => obj.map(s_new),
-                None => s_new
-            };
-            s_orig = s_new;
+            s_new = apply_map(s_new, m);
         }
         s_new
     }).min().unwrap();
 
     println!("Solution 1: {}", smallest_seed);
 
-    let smallest_range: usize = seeds.chunks(2).map(|s| {
-        let mut s_orig = s[0];
-        let mut s_new = s[0];
-        for m in maps.iter() {
-            let o = m.iter().find(|o| o.map_ranged(s_new, s[1]) != s_orig);
-            s_new = match o {
-                Some(obj) => obj.map(s_new),
-                None => s_new
-            };
-            s_orig = s_new;
+    let seed_ranges: Vec<&[usize]> = seeds.chunks(2).collect();
+
+    let mut i: usize = 0;
+    let smallest_range: usize = loop {
+        let mut s_new = i;
+        for map in maps.iter().rev() {
+            s_new = apply_map_rev(s_new, map);
         }
-        s_new
-    }).min().unwrap();
+        if within_range(s_new, &seed_ranges) {
+            break i;
+        }
+        i += 1;
+    };
 
     println!("Solution 2: {}", smallest_range);
 }
